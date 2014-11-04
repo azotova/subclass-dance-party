b6196ec25eca83faf5f4cdad417f05db583782d1
azotova <azotova@gmail.com>
2014-11-03 21:30:50 -0800

    day 1 complete

---
+++ src/jumpyDancer.js
@@ -0,0 +1,18 @@
+var makeJumpingDancer = function(top, left, timeBetweenSteps){
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node.addClass("jumpy");
+
+};
+
+makeJumpingDancer.prototype = Object.create(makeDancer.prototype);
+makeJumpingDancer.prototype.constructor = makeJumpingDancer;
+makeJumpingDancer.prototype.oldStep = function () {
+  return makeDancer.prototype.step.call(this);
+};
+makeJumpingDancer.prototype.step = function () {
+
+  this.oldStep();
+  this.$node.toggleClass('jump');
+
+};
+
